8a875ade7bb477885793a7c309f9aff8c5193c5f
mormushka <roman_tikhonov_0_0@mail.ru>
2024-04-20 00:45:36 +0700

    finaly

--- lab11/src/main.c
+++ lab11/src/main.c
@@ -1,79 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printItems(const int n, const int w, int *weight, int  *cost, void *dpp)
+void printItems(const size_t n, const size_t w, size_t *weight, size_t *cost, void *tc_void)
 {
-    int (*dp)[w + 1] = dpp;
-    int *r_weight = calloc(n, sizeof(int));
-    int *r_cost = calloc(n, sizeof(int));
-
-    printf("%d\n", dp[n][w]);
-
-    int j = w;
-    for (int i = n; i > 0; --i)
+    size_t (*tc)[w + 1] = tc_void;
+    size_t *r_weight = calloc(n, sizeof(size_t));
+    if (r_weight == NULL)
     {
-        if (dp[i][j] != dp[i - 1][j])
+        free(tc);
+        fprintf(stderr, "Memory allocation failed %d\n", __LINE__);
+        return EXIT_FAILURE;
+    }
+    size_t *r_cost = calloc(n, sizeof(size_t));
+    if (r_cost == NULL)
+    {
+        free(r_weight);
+        free(tc);
+        fprintf(stderr, "Memory allocation failed %d\n", __LINE__);
+        return EXIT_FAILURE;
+    }
+
+    printf("%zu\n", tc[n][w]);
+
+    size_t j = w;
+    for (size_t i = n; i > 0; --i)
+    {
+        if (tc[i][j] != tc[i - 1][j])
         {   
             r_weight[i - 1] = weight[i - 1];
             r_cost[i - 1] = cost[i - 1];
             j -= weight[i - 1];
         }
     }
-    for (int i = 0; i < n; ++i)
+
+    for (size_t i = 0; i < n; ++i)
     {
         if (r_weight[i] != 0)
         {
-            printf("%d %d\n", r_weight[i], r_cost[i]);
+            printf("%zu %zu\n", r_weight[i], r_cost[i]);
         }
     }
+
+    free(r_weight);
+    free(r_cost);
+    free(tc);
 }
 
-void knapsack(const int n, const int w, int *weight, int *cost)
+void knapsack(const size_t n, const size_t w, size_t *weight, size_t *cost)
 {
-    int dp[n + 1][w + 1];
-
-    for (int i = 0; i <= n; ++i)
+    size_t (*tc)[w + 1] = calloc( (n + 1) * (w + 1), sizeof(size_t));
+    if (tc == NULL)
     {
-        for (int j = 0; j <= w; ++j)
+        fprintf(stderr, "Memory allocation failed %d\n", __LINE__);
+        return EXIT_FAILURE;
+    }
+
+
+    for (size_t i = 1; i <= n; ++i)
+    {
+        for (size_t j = 1; j <= w; ++j)
         {
-            if ((i == 0) || (j == 0))
+            if (weight[i - 1] <= j)
             {
-                dp[i][j] = 0;
-            }
-            else if (weight[i - 1] <= j)
-            {
-                int previous_maximum = dp[i - 1][j];
-                int cost_with_free_space = cost[i - 1] + dp[i - 1][j - weight[i - 1]];
-                dp[i][j] = (previous_maximum > cost_with_free_space) ? previous_maximum : cost_with_free_space;
+                size_t previous_maximum = tc[i - 1][j];
+                size_t cost_with_free_space = cost[i - 1] + tc[i - 1][j - weight[i - 1]];
+                tc[i][j] = (previous_maximum > cost_with_free_space) ? previous_maximum : cost_with_free_space;
             }
             else
             {
-                dp[i][j] = dp[i - 1][j];
+                tc[i][j] = tc[i - 1][j];
             }
         }
     }
 
-    printItems(n, w, weight, cost, dp);
+    printItems(n, w, weight, cost, tc);
 }
 
 
 int main()
 {
-    //FILE *in = freopen("../in.txt", "r", stdin);
-    int n, w;
-    if (scanf("%d %d", &n, &w) < 2)
+    size_t n, w;
+    if (scanf("%zu %zu", &n, &w) < 2)
     {
         fprintf(stderr, "Input error %d\n", __LINE__);
         return EXIT_FAILURE;
     }
 
-    int weight[n], cost[n];
-    for (int i = 0; i < n; ++i)
+    size_t weight[n], cost[n];
+    for (size_t i = 0; i < n; ++i)
     {
-        scanf("%d %d", &weight[i], &cost[i]);
+        if (scanf("%zu %zu", &weight[i], &cost[i]) < 2)
+        {
+            fprintf(stderr, "Input error %d\n", __LINE__);
+            return EXIT_FAILURE;
+        }
     }
 
     knapsack(n, w, weight, cost);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
