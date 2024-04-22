4572deb2b25c2726a066d5302fb3ec6c9b151618
mormushka <roman_tikhonov_0_0@mail.ru>
2024-04-23 00:12:09 +0700

    finaly no files

--- lab9/src/main.c
+++ lab9/src/main.c
@@ -3,13 +3,65 @@
 #include <limits.h>
 #include <stdint.h>
 
-#define DEBUG1
-
 typedef struct list
 {
     int value;
     struct list *next;
 } list;
+
+void push(list **l, int value);
+void destroy_list(list *l);
+
+typedef struct shortest_paths
+{
+    uint64_t *costs;
+    list **parents;
+    int num_vertices;
+    int start;
+} shortest_paths;
+
+int is_bad_vertex(const unsigned v, const unsigned num_vertices);
+int input_line(unsigned *s_edge, unsigned *f_edge, unsigned *len_edge, const unsigned num_vertices);
+void *input_graph(const unsigned num_vertices, const unsigned start, const unsigned finish, const unsigned num_edges);
+
+int find_lovest_cost_node(uint64_t *costs, char *processed, const int num_vertices);
+void init_costs(uint64_t *costs, const unsigned num_vertices);
+shortest_paths *dijkstra_algorithm(const int start, void *v_graph, const int num_vertices);
+void destroy_shortest_path(shortest_paths *s_paths);
+
+int check_num_paths(list **parents, int f);
+void print_path_array(shortest_paths *s_paths);
+void print_shortest_path(shortest_paths *s_paths, const int finish);
+
+int main()
+{
+    unsigned num_vertices, start, finish, num_edges;
+    if (scanf("%u %u %u %u", &num_vertices, &start, &finish, &num_edges) < 4)
+    {
+        fprintf(stderr, "Input error %d\n", __LINE__);
+        return EXIT_FAILURE;
+    }
+
+    unsigned(*graph)[num_vertices + 1] = input_graph(num_vertices, start, finish, num_edges);
+    if (graph == NULL)
+    {
+        free(graph);
+        return EXIT_SUCCESS;
+    }
+
+    shortest_paths *s_paths = dijkstra_algorithm(start, graph, num_vertices);
+
+    free(graph);
+
+    print_path_array(s_paths);
+    print_shortest_path(s_paths, finish);
+
+    destroy_shortest_path(s_paths);
+
+    return EXIT_SUCCESS;
+}
+
+/*-----------------------------------------List-----------------------------------------*/
 
 void push(list **l, int value)
 {
@@ -19,24 +71,24 @@ void push(list **l, int value)
     *l = new;
 }
 
-typedef struct path
+void destroy_list(list *l)
 {
-    uint64_t *costs;
-    int *parents;
-} path;
-
-typedef struct dijkstra_protocol
-{
-    uint64_t *costs;
-    list **parents;
-} dijkstra_protocol;
-
-int is_bad_vertex(const unsigned v, const unsigned N)
-{
-    return (v == 0) || (v > N);
+    while (l != NULL)
+    {
+        list *next = l->next;
+        free(l);
+        l = next;
+    }
 }
 
-int input_line(unsigned *s_edge, unsigned *f_edge, unsigned *len_edge, const unsigned N)
+/*-----------------------------------------Input-graph-----------------------------------------*/
+
+int is_bad_vertex(const unsigned v, const unsigned num_vertices)
+{
+    return (v == 0) || (v > num_vertices);
+}
+
+int input_line(unsigned *s_edge, unsigned *f_edge, unsigned *len_edge, const unsigned num_vertices)
 {
     int a = scanf("%u %u %u", s_edge, f_edge, len_edge);
     if (a == EOF)
@@ -51,7 +103,7 @@ int input_line(unsigned *s_edge, unsigned *f_edge, unsigned *len_edge, const uns
         return 0;
     }
 
-    if (is_bad_vertex(*s_edge, N) || is_bad_vertex(*f_edge, N))
+    if (is_bad_vertex(*s_edge, num_vertices) || is_bad_vertex(*f_edge, num_vertices))
     {
         printf("bad vertex");
         return 0;
@@ -66,52 +118,54 @@ int input_line(unsigned *s_edge, unsigned *f_edge, unsigned *len_edge, const uns
     return 1;
 }
 
-void *input_graph(const unsigned N, const unsigned S, const unsigned F, const unsigned M)
+void *input_graph(const unsigned num_vertices, const unsigned start, const unsigned finish, const unsigned num_edges)
 {
-    if (N > 5000)
+    if (num_vertices > 5000)
     {
         printf("bad number of vertices");
         return NULL;
     }
 
-    if (is_bad_vertex(S, N) || is_bad_vertex(F, N))
+    if (is_bad_vertex(start, num_vertices) || is_bad_vertex(finish, num_vertices))
     {
         printf("bad vertex");
         return NULL;
     }
 
-    if (M > N * (N + 1) / 2)
+    if (num_edges > num_vertices * (num_vertices + 1) / 2)
     {
         printf("bad number of edges");
         return NULL;
     }
 
-    unsigned(*adjacency_matrix)[N + 1] = calloc((N + 1) * (N + 1), sizeof(unsigned));
+    unsigned(*graph)[num_vertices + 1] = calloc((num_vertices + 1) * (num_vertices + 1), sizeof(unsigned));
 
-    for (unsigned i = 0; i < M; ++i)
+    for (unsigned i = 0; i < num_edges; ++i)
     {
         unsigned s_edge, f_edge;
         unsigned len_edge;
 
-        if (!input_line(&s_edge, &f_edge, &len_edge, N))
+        if (!input_line(&s_edge, &f_edge, &len_edge, num_vertices))
         {
-            free(adjacency_matrix);
+            free(graph);
             return NULL;
         }
 
-        adjacency_matrix[s_edge][f_edge] = len_edge;
-        adjacency_matrix[f_edge][s_edge] = len_edge;
+        graph[s_edge][f_edge] = len_edge;
+        graph[f_edge][s_edge] = len_edge;
     }
 
-    return adjacency_matrix;
+    return graph;
 }
 
-int find_lovest_cost_node(uint64_t *costs, char *processed, const int N)
+/*-----------------------------------------Dijksrta-algoritm-----------------------------------------*/
+
+int find_lovest_cost_node(uint64_t *costs, char *processed, const int num_vertices)
 {
     int lovest_cost_node = 0;
     uint64_t lovest_cost = UINT64_MAX;
 
-    for (int i = 1; i <= N; ++i)
+    for (int i = 1; i <= num_vertices; ++i)
     {
         if ((!processed[i]) && (costs[i] < lovest_cost))
         {
@@ -123,21 +177,21 @@ int find_lovest_cost_node(uint64_t *costs, char *processed, const int N)
     return lovest_cost_node;
 }
 
-void init_costs(uint64_t *costs, const unsigned N)
+void init_costs(uint64_t *costs, const unsigned num_vertices)
 {
-    for (unsigned i = 1; i <= N; ++i)
+    for (unsigned i = 1; i <= num_vertices; ++i)
     {
         costs[i] = UINT64_MAX;
     }
 }
 
-dijkstra_protocol *dijkstra_algorithm(const unsigned S, void *a_matrix, const unsigned N)
+shortest_paths *dijkstra_algorithm(const int start, void *v_graph, const int num_vertices)
 {
-    unsigned(*adjacency_matrix)[N + 1] = a_matrix;
+    unsigned(*graph)[num_vertices + 1] = v_graph;
 
-    uint64_t *costs = calloc(N + 1, sizeof(uint64_t));
-    list **parents = calloc(N + 1, sizeof(list));
-    char *processed = calloc(N + 1, sizeof(char));
+    uint64_t *costs = calloc(num_vertices + 1, sizeof(uint64_t));
+    list **parents = calloc(num_vertices + 1, sizeof(list));
+    char *processed = calloc(num_vertices + 1, sizeof(char));
     if ((costs == NULL) || (parents == NULL) || (processed == NULL))
     {
         free(costs);
@@ -147,82 +201,51 @@ dijkstra_protocol *dijkstra_algorithm(const unsigned S, void *a_matrix, const un
         return NULL;
     }
 
-    init_costs(costs, N);
-    costs[S] = 0;
+    init_costs(costs, num_vertices);
+    costs[start] = 0;
 
-    int node = find_lovest_cost_node(costs, processed, N);
+    int node = find_lovest_cost_node(costs, processed, num_vertices);
     while (node)
     {
         uint64_t cost_node = costs[node];
-        for (unsigned i = 1; i <= N; ++i)
+        for (int i = 1; i <= num_vertices; ++i)
         {
-            if (adjacency_matrix[node][i])
+            if (graph[node][i])
             {
-                uint64_t new_cost = cost_node + adjacency_matrix[node][i];
+                uint64_t new_cost = cost_node + graph[node][i];
                 if (new_cost <= costs[i])
                 {
                     costs[i] = new_cost;
-                    // parents[i] = node;
                     push(&parents[i], node);
                 }
             }
         }
         processed[node] = 1;
-        node = find_lovest_cost_node(costs, processed, N);
+        node = find_lovest_cost_node(costs, processed, num_vertices);
     }
 
-#ifdef DEBUG
-    printf("costs:     ");
-    for (int i = 1; i <= N; ++i)
-    {
-        if (costs[i] != UINT64_MAX)
-        {
-            printf("%lu ", costs[i]);
-        }
-        else
-        {
-            printf("I ");
-        }
-    }
-    printf("\n");
-
-    printf("parents:   ");
-    for (int i = 1; i <= N; ++i)
-    {
-        if (parents[i])
-        {
-            // printf("%d ", parents[i]->value);
-            list *tmp = parents[i];
-            while (tmp != NULL)
-            {
-                printf("|%d|", tmp->value);
-                tmp = tmp->next;
-            }
-        }
-        else
-        {
-            printf("n ");
-        }
-    }
-    printf("\n");
-
-    printf("processed: ");
-    for (int i = 1; i <= N; ++i)
-    {
-        printf("%d ", processed[i]);
-    }
-    printf("\n");
-#endif
-
-    dijkstra_protocol *d_protocol = malloc(sizeof(dijkstra_protocol));
+    shortest_paths *d_protocol = malloc(sizeof(shortest_paths));
     d_protocol->costs = costs;
     d_protocol->parents = parents;
-    // free(costs);
-    // free(parents);
+    d_protocol->num_vertices = num_vertices;
+    d_protocol->start = start;
     free(processed);
     return d_protocol;
 }
 
+void destroy_shortest_path(shortest_paths *s_paths)
+{
+    free(s_paths->costs);
+    for (int i = 1; i <= s_paths->num_vertices; ++i)
+    {
+        list *tmp = s_paths->parents[i];
+        destroy_list(tmp);
+    }
+    free(s_paths->parents);
+    free(s_paths);
+}
+
+/*-----------------------------------------Output-----------------------------------------*/
 int check_num_paths(list **parents, int f)
 {
     if (parents[f] == NULL)
@@ -242,110 +265,50 @@ int check_num_paths(list **parents, int f)
     }
 }
 
-void print_dijkstra(dijkstra_protocol *d_protocol, const int N, const int S, const int F)
+void print_path_array(shortest_paths *s_paths)
 {
-    for (int i = 1; i <= N; ++i)
+    for (int i = 1; i <= s_paths->num_vertices; ++i)
     {
-        if (d_protocol->costs[i] == UINT64_MAX)
+        if (s_paths->costs[i] == UINT64_MAX)
         {
             printf("oo ");
         }
-        else if (d_protocol->costs[i] > INT_MAX)
+        else if (s_paths->costs[i] > INT_MAX)
         {
             printf("INT_MAX+ ");
         }
         else
         {
-            printf("%lu ", d_protocol->costs[i]);
+            printf("%lu ", s_paths->costs[i]);
         }
     }
-
     printf("\n");
+}
 
-    if (d_protocol->costs[F] == UINT64_MAX)
+void print_shortest_path(shortest_paths *s_paths, const int finish)
+{
+    if (s_paths->costs[finish] == UINT64_MAX)
     {
-        printf("no path");
+        printf("no path\n");
+        return;
     }
-    else if (d_protocol->costs[F] > INT_MAX)
+    else if (s_paths->costs[finish] > INT_MAX)
     {
-        int num_paths = check_num_paths(d_protocol->parents, F);
+        int num_paths = check_num_paths(s_paths->parents, finish);
 
         if (num_paths > 1)
         {
-            printf("overflow");
-        }
-        else
-        {
-            int i = F;
-            printf("%d ", i);
-            while (i != S)
-            {
-                i = d_protocol->parents[i]->value;
-                printf("%d ", i);
-            }
+            printf("overflow\n");
+            return;
         }
     }
-    else
+
+    int i = finish;
+    printf("%d ", i);
+    while (i != s_paths->start)
     {
-        int i = F;
+        i = s_paths->parents[i]->value;
         printf("%d ", i);
-        while (i != S)
-        {
-            i = d_protocol->parents[i]->value;
-            printf("%d ", i);
-        }
     }
-    free(d_protocol->costs);
-    for (int i = 1; i <= N; ++i)
-    {
-        list *tmp = d_protocol->parents[i];
-        while (tmp != NULL)
-        {
-            list *next = tmp->next;
-            free(tmp);
-            tmp = next;
-        }
-    }
-    free(d_protocol->parents);
-    free(d_protocol);
-}
-
-int main()
-{
-#ifdef DEBUG
-    FILE *in = freopen("../in.txt", "r", stdin);
-#endif
-
-    unsigned N, S, F, M;
-    if (scanf("%u %u %u %u", &N, &S, &F, &M) < 4)
-    {
-        fprintf(stderr, "Input error %d\n", __LINE__);
-        return EXIT_FAILURE;
-    }
-
-    unsigned(*adjacency_matrix)[N + 1] = input_graph(N, S, F, M);
-    if (adjacency_matrix == NULL)
-    {
-        free(adjacency_matrix);
-        return EXIT_SUCCESS;
-    }
-
-    print_dijkstra(dijkstra_algorithm(S, adjacency_matrix, N), N, S, F);
-
-#ifdef DEBUG
-    printf("             1 2 3 4 5 6 7 8 9 0\n");
-    printf("--------------------------------\n");
-    for (int i = 1; i < N + 1; ++i)
-    {
-        printf("children %d : ", i);
-        for (int j = 1; j < N + 1; ++j)
-        {
-            printf("%u ", adjacency_matrix[i][j]);
-        }
-        printf("\n");
-    }
-#endif
-
-    free(adjacency_matrix);
-    return EXIT_SUCCESS;
+    printf("\n");
 }
