fffdbbd8bbae6710e9ec38cb81750f92e3f52975
mormushka <roman_tikhonov_0_0@mail.ru>
2024-04-26 02:06:49 +0700

    finally

--- lab6-2/src/main.c
+++ lab6-2/src/main.c
@@ -5,11 +5,12 @@
 
 typedef struct b_tree
 {
-    int n;
+    int n; /*фактическое количество ключей*/
     int *key;
     struct b_tree **child;
 } b_tree;
 
+/*бинарный поиск (если значение меньше всех 0, если больше всех n)*/
 int get_child_idx(const b_tree *bt, int k)
 {
     int left = 0;
@@ -33,26 +34,39 @@ int get_child_idx(const b_tree *bt, int k)
     return left;
 }
 
-void move_tail(b_tree *src, int i, b_tree *dst, int j)
+void move_tail(b_tree *dest, int i, b_tree *src, int j)
 {
-    int t = dst->n - j;
-    memmove(dst->key + j, src->key + i, t * sizeof(int));
-    memmove(dst->child + j, src->child + i, (t + 1) * sizeof(b_tree *));
+    int t = dest->n - i;
+    memmove(dest->key + i, src->key + j, t * sizeof(int));
+    memmove(dest->child + i, src->child + j, (t + 1) * sizeof(b_tree *));
 }
 
 void add_key(b_tree *bt, int k)
 {
     int c = get_child_idx(bt, k);
     bt->n += 1;
-    memmove(bt->key + c + 1, bt->key + c, (bt->n - c - 1) * sizeof(int));
+    move_tail(bt, c + 1, bt, c);
     bt->key[c] = k;
 }
 
 b_tree *create(int t)
 {
-    b_tree *tmp = malloc(sizeof(b_tree));
-    tmp->key = malloc((2 * t - 1) * sizeof(int));
-    tmp->child = malloc(2 * t * sizeof(b_tree *));
+    b_tree *tmp = calloc(1, sizeof(b_tree));
+    if (tmp == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed %d\n", __LINE__);
+        return NULL;
+    }
+    tmp->key = calloc(2 * t - 1, sizeof(int));
+    tmp->child = calloc(2 * t, sizeof(b_tree *));
+    if ((tmp->key == NULL) || (tmp->child == NULL))
+    {
+        free(tmp->key);
+        free(tmp->child);
+        free(tmp);
+        fprintf(stderr, "Memory allocation failed %d\n", __LINE__);
+        return NULL;
+    }
     return tmp;
 }
 
@@ -60,11 +74,16 @@ void split(int c, b_tree *p, int t)
 {
     b_tree *fc = p->child[c];
     b_tree *nc = create(t);
+
     nc->n = t - 1;
-    move_tail(fc, t, nc, 0);
     fc->n = t - 1;
+
+    move_tail(nc, 0, fc, t);
+
     p->n += 1;
-    move_tail(p, c, p, c + 1);
+
+    move_tail(p, c + 1, p, c);
+
     p->key[c] = fc->key[t - 1];
     p->child[c + 1] = nc;
 }
@@ -82,13 +101,15 @@ bool is_full(b_tree *bt, int t)
 void insert_non_full(b_tree *bt, int t, int k)
 {
     if (is_leaf(bt))
+    {
         add_key(bt, k);
+    }
     else
     {
-        int childIdx = get_child_idx(bt, k);
-        if (is_full(bt->child[childIdx], t))
+        int child_idx = get_child_idx(bt, k);
+        if (is_full(bt->child[child_idx], t))
         {
-            split(childIdx, bt, t);
+            split(child_idx, bt, t);
         }
         insert_non_full(bt->child[get_child_idx(bt, k)], t, k);
     }
@@ -104,13 +125,12 @@ void insert(b_tree **bt, int t, int k)
     }
     else
     {
-        b_tree *nonFull = *bt;
-        if (is_full(nonFull, t))
+        if (is_full(*bt, t))
         {
-            b_tree *newRoot = create(t);
-            newRoot->child[0] = nonFull;
-            split(0, newRoot, t);
-            *bt = newRoot;
+            b_tree *new_root = create(t);
+            new_root->child[0] = *bt;
+            split(0, new_root, t);
+            *bt = new_root;
         }
         insert_non_full(*bt, t, k);
     }
@@ -122,13 +142,13 @@ b_tree *input_tree(int t, int tree_size)
 
     for (int i = 0; i < tree_size; ++i)
     {
-        int value = 0;
-        if (scanf("%d", &value) <= 0)
+        int k = 0;
+        if (scanf("%d", &k) <= 0)
         {
             fprintf(stderr, "Input error %d\n", __LINE__);
         }
 
-        insert(&bt, t, value);
+        insert(&bt, t, k);
     }
 
     return bt;
