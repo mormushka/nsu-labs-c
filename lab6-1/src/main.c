63b0d4d1cb28e915965eb9ab2456818667ab414f
mormushka <roman_tikhonov_0_0@mail.ru>
2024-04-28 19:51:55 +0700

    df

--- lab6-1/src/main.c
+++ lab6-1/src/main.c
@@ -18,6 +18,7 @@ typedef struct rbt
 {
     int value;
     char color;
+    struct rbt *parent;
     struct rbt *left;
     struct rbt *right;
 } rbt;
@@ -63,6 +64,7 @@ rbt *create_leaf(int value, t_memory *memory)
 
     new->value = value;
     new->color = RED;
+    new->parent = NULL;
     new->left = NULL;
     new->right = NULL;
 
@@ -74,11 +76,12 @@ char color(rbt *t)
     return t ? t->color : BLACK;
 }
 
-void swap_color(rbt *t)
+rbt *rotate_right(rbt *t)
 {
-    t->color = RED;
-    t->left->color = BLACK;
-    t->right->color = BLACK;
+    rbt *buffer = t->left;
+    t->left = buffer->right;
+    buffer->right = t;
+    return buffer;
 }
 
 rbt *rotate_left(rbt *t)
@@ -86,64 +89,27 @@ rbt *rotate_left(rbt *t)
     rbt *buffer = t->right;
     t->right = buffer->left;
     buffer->left = t;
-
-    buffer->color = t->color;
-    t->color = RED;
-
     return buffer;
 }
 
-rbt *rotate_right(rbt *t)
+void insert(int value, rbt **tree, t_memory *memory)
 {
-    rbt *buffer = t->left;
-    t->left = buffer->right;
-    buffer->right = t;
-
-    buffer->color = t->color;
-    t->color = RED;
-
-    if ((color(buffer->left) == RED) && (color(buffer->right) == RED))
+    rbt* new_node = create_leaf(value, memory);
+    rbt *current_node = &tree;
+    rbt *current_parent = NULL;
+    while (current_node != NULL)
     {
-        swap_color(buffer);
+        current_parent = current_node;
+        if (value < current_node->value)
+        {
+            current_parent = current_node->left;
+        }
+        else
+        {
+            current_parent = current_node->right;
+        }
     }
 
-    return buffer;
-}
-
-rbt *balance(rbt *t)
-{
-    if ((color(t->right) == RED) && (color(t->left) == BLACK))
-    {
-        return rotate_left(t);
-    }
-    if ((color(t->left) == RED) && (color(t->left->left) == RED)) ///
-    {
-        return rotate_right(t);
-    }
-    if ((color(t->left) == RED) && (color(t->right) == RED))
-    {
-        swap_color(t);
-    }
-    
-    return t;
-}
-
-void insert(int value, rbt **t, t_memory *memory)
-{
-    if (!(*t))
-    {
-        *t = create_leaf(value, memory);
-    }
-    else if (value < (*t)->value)
-    {
-        insert(value, &(*t)->left, memory);
-    }
-    else
-    {
-        insert(value, &(*t)->right, memory);
-    }
-
-    *t = balance(*t);
 }
 
 rbt *input_tree(int tree_size, t_memory *memory)
@@ -160,20 +126,17 @@ rbt *input_tree(int tree_size, t_memory *memory)
 
         insert(value, &tree, memory);
     }
-    if (tree)
-    {
-        tree->color = BLACK;
-    }
+
     return tree;
 }
 
-int height(rbt *t) 
+int height(rbt *t)
 {
     if (!t)
     {
         return 1;
     }
-    
+
     return (t->color == BLACK) + height(t->right);
 }
 
