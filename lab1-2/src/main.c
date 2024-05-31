cc37ccb190039b77abbd70ae93ef805d2cbf10e0
mormushka <roman_tikhonov_0_0@mail.ru>
2024-05-31 22:55:05 +0700

    f

--- lab1-2/src/main.c
+++ lab1-2/src/main.c
@@ -1,190 +1,138 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
-#define MAX_SAMPLE_LEN 16
-#define MAX_STR_LEN 256
+#define MAX_P_LEN 16
+#define BUFFER_SIZE 32
 
-typedef struct t_string
+typedef struct
 {
-    int index;
-    int len;
-    char *line;
-} t_string;
+    size_t data[BUFFER_SIZE];
+    size_t first;
+    size_t free_block;
+    int count;
+} queue;
 
-t_string create_string(int len);
-int input_sample(t_string *str);
-void input_str(t_string *str);
-void prefix_function(int *shift_table, t_string *sample);
-void print_shift_table(int sample_len, int *shift_table);
-void print_protokol(int position, int len);
-int shift_str(int shift, int sample_len, t_string *string);
-int kmp(t_string *sample);
-
-int main()
+void queue_add(queue *q, size_t index, size_t prefix_len)
 {
-    //FILE *in = freopen("../in.txt", "r", stdin);
-    t_string sample = create_string(MAX_SAMPLE_LEN);
-    if (sample.line == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed %d\n", __LINE__);
-    }
-
-    if (input_sample(&sample) == EXIT_FAILURE)
-    {
-        free(sample.line);
-        return EXIT_FAILURE;
-    }
-
-    if (kmp(&sample) == EXIT_FAILURE)
-    {
-        free(sample.line);
-        return EXIT_FAILURE;
-    }
-
-    free(sample.line);
-    return EXIT_SUCCESS;
+    q->data[q->free_block++] = index;
+    q->data[q->free_block++] = prefix_len;
+    q->free_block %= BUFFER_SIZE;
+    q->count++;
 }
 
-t_string create_string(int len)
+void del_first(queue *q)
 {
-    t_string tmp = {
-        .index = 0,
-        .len = 0,
-        .line = (char *)malloc(len * sizeof(char))};
-
-    return tmp;
+    q->first = (q->first + 2) % BUFFER_SIZE;
+    q->count--;
 }
 
-int input_sample(t_string *str)
+void fill_shift_table(int *shift_table, const char *pattern, int len)
 {
-    for (int i = 0; i < MAX_SAMPLE_LEN + 1; ++i)
+    int i = 1;
+    int j = 0;
+    while (len > i)
     {
-        int c = 0;
-        if (fread(&c, sizeof(char), 1, stdin) != 1)
+        if (pattern[j] == pattern[i])
         {
-            fprintf(stderr, "Input error %d\n", __LINE__);
-            return EXIT_FAILURE;
+            shift_table[i] = ++j;
+            ++i;
         }
-
-        if (c == '\n')
+        else
         {
-            str->len = i;
-            return EXIT_SUCCESS;
+            if (j == 0)
+            {
+                shift_table[i] = 0;
+                ++i;
+            }
+            else
+            {
+                j = shift_table[j - 1];
+            }
         }
-
-        str->line[i] = c;
     }
-
-    fprintf(stderr, "Sample lenght > MAX_SAMPLE_LEN %d\n", __LINE__);
-    return EXIT_FAILURE;
 }
 
-void input_str(t_string *str)
+void kmp(FILE *in)
 {
-    str->len = (int)fread(str->line, sizeof(char), MAX_STR_LEN, stdin);
-    str->index = 0;
-}
-
-void prefix_function(int *shift_table, t_string *sample)
-{
-    for (int i = 1; i < sample->len; ++i)
+    char pattern[MAX_P_LEN + 2] = {0};
+    if (fgets(pattern, MAX_P_LEN + 2, in) == 0)
     {
-        int j = shift_table[i - 1];
-        while ((j > 0) && (sample->line[i] != sample->line[j]))
-        {
-            j = shift_table[j - 1];
-        }
-
-        if (sample->line[i] == sample->line[j])
-        {
-            ++j;
-        }
-
-        shift_table[i] = j;
+        return;
     }
-}
 
-void print_shift_table(int sample_len, int *shift_table)
-{
-    for (int i = 0; i < sample_len; ++i)
+    int len = 0;
+    len = strlen(pattern) - 1;
+    if (len == 0)
+    {
+        return;
+    }
+
+    int shift_table[MAX_P_LEN] = {0};
+    fill_shift_table(shift_table, pattern, len);
+
+    for (int i = 0; i < len; ++i)
     {
         printf("%d ", shift_table[i]);
     }
-}
 
-void print_protokol(int position, int len)
-{
-    printf("%d %d ", position, len);
-}
-
-int shift_str(int shift, int sample_len, t_string *string)
-{
-    if (string->index + sample_len + shift> string->len)
+    queue q = {
+        .count = 0,
+        .first = 0,
+        .free_block = 0
+    };
+    size_t i = 1;
+    int j = 0;
+    int c = fgetc(in);
+    while (c != EOF)
     {
-        int shifted_sum_count = string->len - string->index - shift;
-        for (int i = 0; i < shifted_sum_count; ++i)
+        if (pattern[j] == (char)c)
         {
-            string->line[i] = string->line[string->index + shift + i];
+            ++i;
+            ++j;
+            c = fgetc(in);
         }
-
-        string->len = shifted_sum_count + (int)fread(string->line + shifted_sum_count, sizeof(char), MAX_STR_LEN - shifted_sum_count, stdin);
-        string->index = 0;
-        return (string->len > sample_len);
+        else
+        {
+            if (j > 0)
+            {
+                queue_add(&q, i - j, j);
+                j = shift_table[j - 1];
+                while (q.count && (q.data[q.first] <= i - len))
+                {
+                    printf("%zu %zu ", q.data[q.first], q.data[q.first + 1]);
+                    del_first(&q);
+                }
+            }
+            else
+            {
+                ++i;
+                c = fgetc(in);
+            }
+        }
     }
 
-    string->index += shift;
-    return 1;
+    if (j == len)
+    {
+        queue_add(&q, i - j, j);
+    }
+    while (q.count && (q.data[q.first] <= i - len))
+    {
+        printf("%zu %zu ", q.data[q.first], q.data[q.first + 1]);
+        del_first(&q);
+    }
 }
 
-int kmp(t_string *sample)
+int main()
 {
-    if (sample->len == 0)
+    FILE *in = fopen("in.txt", "r");
+    if (in == NULL)
     {
-        fprintf(stderr, "Sample lenght = 0 %d\n", __LINE__);
-        return EXIT_SUCCESS;
-    }
-
-    int shift_table[MAX_SAMPLE_LEN] = {0};
-    prefix_function(shift_table, sample);
-    print_shift_table(sample->len, shift_table);
-
-    t_string str = create_string(MAX_STR_LEN);
-    if (str.line == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed %d\n", __LINE__);
         return EXIT_FAILURE;
     }
+    kmp(in);
 
-    input_str(&str);
-
-    if (sample->len > str.len)
-    {
-        free(str.line);
-        return EXIT_SUCCESS;
-    }
-
-    int position = 1;
-
-    for (;;)
-    {
-        int prefix_len = 0;
-        while ((prefix_len < sample->len) && (sample->line[prefix_len] == str.line[str.index + prefix_len]))
-        {
-            ++prefix_len;
-        }
-
-        int shift = 1;
-        if (prefix_len != 0)
-        {
-            shift = prefix_len - shift_table[prefix_len - 1];
-            print_protokol(position, prefix_len);
-        }
-
-        position += shift;
-        if (!shift_str(shift, sample->len, &str))
-        {
-            free(str.line);
-            return EXIT_SUCCESS;
-        }
-    }
+    fclose(in);
+    return EXIT_SUCCESS;
 }
